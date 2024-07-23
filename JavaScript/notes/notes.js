eddbb292a84d227718522920dd43299992ac407e
Neeraja-Turupati <tneeraja20@gmail.com>
2024-07-23 14:15:49 +0530

    Project Done

---
+++ JavaScript/notes/notes.js
@@ -0,0 +1,15 @@
+//  UNARY OPERATOR:
+//unary plus ==> used to convert any value into  number
+console.log(+1,+'',+true, +'prasad');
+a=25;
+console.log(a)
+++a and a++;
+console.log(a)
+a-- and --a;
+console.log(a)
+
+//there are two way increment or decrement happens, oone is pre increment another is post increment
+//++a is pre increment, a++ is post ==> pre increment first update the value and returens , post increment first returens the value and updates.
+
+var b=12;
+var x= ++b + b++ -b++ + ++b;  //13 +13 -14 + 16
